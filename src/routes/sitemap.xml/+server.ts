b624ba4e0081295c7e64122add4f21f2647ab867
Berat Hündürel <berat.hundurel@bilgiedu.net>
2026-07-20 23:35:41 +0300

    update last-mod

--- src/routes/sitemap.xml/+server.ts
+++ src/routes/sitemap.xml/+server.ts
@@ -7,7 +7,7 @@ export const GET: RequestHandler = () => {
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
 	<loc>${siteUrl}/</loc>
-	<lastmod>2026-02-23</lastmod>
+	<lastmod>2026-07-20</lastmod>
 	<changefreq>monthly</changefreq>
 	<priority>1.0</priority>
   </url>
